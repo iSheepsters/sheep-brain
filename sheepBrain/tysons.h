4f2cb831ed44f2b5c1003269fd498269f42e86d1
Bill Pugh <pugh@cs.umd.edu>
2019-12-23 09:45:02 -0500

    Not a Tyson's sheep anymore

--- sheepBrain/tysons.h
+++ sheepBrain/tysons.h
@@ -1,7 +1,7 @@
 #ifndef _TYSONS_H
 #define _TYSONS_H
 
-#define MALL_SHEEP  true
+#define MALL_SHEEP  false
 extern bool isOpen(bool debug);
 extern bool isShutdown(bool debug);
 extern int minutesUntilOpen(bool debug);
